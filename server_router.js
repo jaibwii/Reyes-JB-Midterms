7df2b103ec80e9a0c5a34c102484dffa1d102ff3
jaibwii <162951817+jaibwii@users.noreply.github.com>
2024-08-05 15:33:22 +0800

    Add files via upload

---
+++ server_router.js
@@ -0,0 +1,25 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req,res){
+    console.log("A POST request for the homepage is accessed.");
+    res.send('Here is the POST Method');
+})
+
+app.get('/list_user', function (req,res) {
+    console.log("Got a GET request for /list_user");
+    res.send('This is a Page Listing');
+})
+
+app.get('/ab*cd', function (req,res) {
+    console.log("Got a GET request for /ab*cd");
+    res.send('Pattern Match Page');
+})
+
+var server = app.listen(4000, function (){
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s",host, port)
+})
